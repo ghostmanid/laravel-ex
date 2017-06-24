4ac32af1fca1ce672b5f54ec87bdbefbde5e2de2
Trinh  Van Trung <trinhvantrung.it@gmail.com>
2017-06-24 10:25:33 +0000

    cap nhat giao dien cho player

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -8,9 +8,10 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <link href="http://vjs.zencdn.net/6.1.0/video-js.css" rel="stylesheet">
 
-    
-    
+    <!-- If you'd like to support IE8 -->
+    <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
 	<style type="text/css">
 		body{
 			
@@ -24,7 +25,7 @@
   		@yield('content')  		
 	
   	</div>
-
+    
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
--- resources/views/xemphim.blade.php
+++ resources/views/xemphim.blade.php
@@ -8,37 +8,46 @@
 			</div>
 		  
 		  <div class="embed-responsive embed-responsive-16by9">
-			   <video  controls="" id="video" class="mejs__player embed-responsive-item">
-				<source type="video/mp4" src="" />
-				</video>
+			   <video   id="video" class="mejs__player embed-responsive-item video-js" controls preload="auto" >
+				
 		</div>  		
 		</div>
+		<div>
 
+ 
 <script type="text/javascript" src="{{asset('/js')}}/js.js"></script>
 <script type="text/javascript">
 	 $(document).ready(function(){
 
 		var code = "{{$data['code']}}";
 		var  id =   {{$data['id']}};
-		var video = document.getElementById('video');
-		var source = document.createElement('source');
+		 var video = document.getElementById('video');
+		 var source = document.createElement('source');
 		
-		source.setAttribute('src', '');
+		// source.setAttribute('src', '');
 
-		video.appendChild(source);
-		video.play();
+		 //video.appendChild(source);
+		 //video.play();
+		 setTimeout(function() {  
+		 //    video.pause();
 
-		setTimeout(function() {  
-		    video.pause();
+		  // source.setAttribute('src', decodeLink(code,id)); 
 
-		   source.setAttribute('src', decodeLink(code,id)); 
-
-		    video.load();
-		    video.play();
-		}, 0);
+		  //   video.load();
+		     var myPlayer = videojs("video");
+			 
+			 myPlayer.src({type: "video/mp4", src: decodeLink(code,id)});
+			 myPlayer.ready(function() {
+			   myPlayer.play();
+			 });
+		     
+		 }, 0);
+		
+		
 
 	});
 
 </script>
+<script src="http://vjs.zencdn.net/6.1.0/video.js"></script> 
 @endif
 @endsection
