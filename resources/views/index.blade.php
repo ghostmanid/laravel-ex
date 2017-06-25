f35565fe1299d4891425a7b3300fb77a38edfe55
Trinh  Van Trung <trinhvantrung.it@gmail.com>
2017-06-25 01:45:28 +0000

    a

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,7 @@
 			  <div class="col-sm-3">
 			    <div class="card">
 			     
-			     <img class="card-img-top" src="{{$value[$key]['img']}}" alt="Card image cap">
+			     <img class="card-img-top" src="{{$value[$key]['img']}}" alt="Card image cap" >
 			     <div class="card-block">
 			        <h3 class="card-title">{{$value[$key]['tieude']}}</h3>
 			        <p class="card-text">{{$value[$key]['label']}}</p>
@@ -41,47 +41,7 @@
 		</div>
 		@endif
 		<script type="text/javascript">
-					// $.ajax({
-					//   "async": true,
-					//   "crossDomain": true,
-					//   "url": "http://media.bilutv.com/uploads/2016/11/150/nazotokine-vietsub-201611772.jpg",
-					//   "method": "GET",
-					//   "headers": {
-					//     "cache-control": "no-cache",
-					//     "postman-token": "c503d2b9-bab9-d2dc-cc2e-064c2f544d1d"
-					   
-					//   },
-					//   success: function() { alert("Success"); },
-     //    			error: function() { alert('Failed!'); },
-					//  beforeSend: setHeader
-					// });
-					// function setHeader(xhr) {
-
-					//   xhr.setRequestHeader('Access-Control-Allow-Origin', "bilutv.com");
-					// }
-											var cor = null; // cor stands for Cross-Origin request
-
-						if (window.XMLHttpRequest) {
-						    cor = new XMLHttpRequest();
-						}
-						//else if (window.XDomainRequest) {
-						    //cor = new XDomainRequest();
-						//}
-						else {
-						    alert("Your browser does not support Cross-Origin request!");
-						    // return;
-						}
-
-						
-						
-						
-						    cor.open('GET', 'http://media.bilutv.com/uploads/2016/11/150/nazotokine-vietsub-201611772.jpg', true);
-						    cor.withCredential = "true";
-						     cor.setRequestHeader('Content-Type', 'text/html; charset=utf-8');
-						     cor.setRequestHeader('Origin', 'http://bilutv.com');
-						      cor.setRequestHeader('Access-Control-Allow-Origin', 'media.bilutv.com');
-						    cor.send(null);
-						
+					
 
 					
 				</script>	
